1ade33af6a39bef04594e65d3fc604d3a35e3310
josemorista <josemorista@id.uff.br>
2019-12-06 13:30:11 -0300

    fixes

--- sources/aiml/greetings.js
+++ sources/aiml/greetings.js
@@ -5,7 +5,7 @@ module.exports = `
 <?xml version="1.0" encoding="UTF-8"?>
 <aiml version="2.0">
 ${patterns(greetingsSmallTalks, { srai: 'INITIAL_OPTIONS', msg: 'Hi!' })}
-${pattern('initial_options', {
+${pattern('INITIAL_OPTIONS', {
   oob: choices(['button1', 'button2'])
 })}
 </aiml>
